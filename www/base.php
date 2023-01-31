d327ae44c93ece2fd335ee8e921a30f10b3499af
DELAPLACE Hugo <o22106071@info22-15.iut45.univ-orleans.fr>
2023-01-31 15:28:06 +0100

    templates php

---
+++ www/base.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quizz</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="accueil.php"></a>
+            <a href="quizz.php"></a>
+            <a href="creer_quizz.php"></a>
+            <a href="contact.php"></a>
+        </nav>
+    </header>
+</body>
+</html>
