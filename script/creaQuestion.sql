8a215faa503243f4bc74e71c6481ac49556bfaba
Bastian <rapine.bastian@gmail.com>
2023-02-06 14:21:33 +0100

    ajout de question de base pour test de l'affichage

--- script/creaQuestion.sql
+++ script/creaQuestion.sql
@@ -1,4 +1,4 @@
-drop table if exist QUESTION, TYPEQUESTION;
+drop table QUESTION, TYPEQUESTION;
 
 create table QUESTION(
     nom varchar(20) primary key,
@@ -15,3 +15,11 @@ create table TYPEQUESTION(
 );
 
 ALTER TABLE QUESTION ADD FOREIGN KEY (idType) REFERENCES TYPEQUESTION(idType);
+
+INSERT INTO TYPEQUESTION VALUES
+(1, 'text'),
+(2, 'radio');
+
+insert into QUESTION values 
+("ultime", 1, "Quelle est la réponse à la question ultime de la vie?",null, "42", 4.2),
+("cheval", 2, "Quelle est la couleur du cheval blanc d'Henry IV ?", "bleu, blanc, rouge", "blanc", 2.0);
