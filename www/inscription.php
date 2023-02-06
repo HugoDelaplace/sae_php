820c63be24191e74648d9b5d5a28f6d0ed128a97
DELAPLACE Hugo <o22106071@info21-17.iut45.univ-orleans.fr>
2023-02-06 15:15:39 +0100

    pages de connexion et inscription

---
+++ www/inscription.php
@@ -0,0 +1,38 @@
+<?php require_once "base.html";?>
+
+<body>
+    <h1>S'inscrire'</h1>
+    <div class="connecter">
+        <form action="inscription.php" method="post">
+            <label for="pseudo">Pseudo :</label>
+            <input type="text" name="pseudo" id="pseudo" required>
+            <label for="mdp">Mot de passe :</label>
+            <input type="password" name="mdp" id="mdp" required>
+            <label for="mdpC">Confirmez le mot de passe :</label>
+            <input type="password" name="mdpC" id="mdpC" required>
+            <input type="submit" value="S'inscrire">
+        </form>
+    </div>
+</body>
+
+<?php require "connexion.php";
+    $db = connectBd();
+    if (isset($_POST["pseudo"]) && isset($_POST["mdp"]) && isset($_POST["mdpC"])) {
+        $pseudo = $_POST["pseudo"];
+        $mdp = $_POST["mdp"];
+        $mdpC = $_POST["mdpC"];
+        if ($mdp == $mdpC) {
+            $sql = "INSERT INTO user (pseudo, mdp) VALUES ('$pseudo', '$mdp')";
+            $result = $db->query($sql);
+            header("Location: connexion_user.php");
+        }
+        else {
+            echo "Erreur";
+            header("Location: inscription.php");
+        }
+    }
+    else {
+        echo "Erreur";
+        header("Location: inscription.php");
+    }
+?>
