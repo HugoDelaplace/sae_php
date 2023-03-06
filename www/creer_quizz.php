9921f7c388726bb33caa389cdd30245ff1673b2e
DELAPLACE Hugo <o22106071@info21-11.iut45.univ-orleans.fr>
2023-03-06 15:28:42 +0100

    probleme insertion bd

--- www/creer_quizz.php
+++ www/creer_quizz.php
@@ -1,13 +1,13 @@
 <?php require_once "base.html";?>
 <body class="quizz">
     <h1>Créer un quizz</h1>
-    <form action="quizz_cree.php" method="post">
+    <form action="quizz_cree.php" method="get">
         <label for="titre">Titre :</label>
         <input type="text" name="titre" id="titre" required>
         <label for="description">Description :</label>
         <input type="text" name="description" id="description">
         <label for="nb_questions">Nombre de questions :</label>
-        <input type="number" name="nb_questions" id="nb_questions" min="1" max="100" required>
+        <input type="number" name="nb_quest" id="nb_quest" min="1" max="100" required>
         <input type="submit" value="Créer">
     </form>
 </body>
--- www/quizz_cree.php
+++ www/quizz_cree.php
@@ -1,12 +1,18 @@
 <?php 
     require_once "connexion.php";
     $db = connectBd();
-    if (!empty($_GET["titre"]) && !empty($_GET["description"]) && !empty($_GET["nb_questions"])) {
+    if (!empty($_GET["titre"]) && !empty($_GET["nb_quest"])) {
         $titre = $_GET["titre"];
         $description = $_GET["description"];
-        $nb_questions = $_GET["nb_questions"];
-        $sql = "INSERT INTO QUIZZ (idQ, titreQ, descriptionQ, nb_questions) VALUES ('$titre', '$description', '$nb_questions')";
-        $db->exec($sql);
+        $nb_questions = $_GET["nb_quest"];
+        $sql = "INSERT INTO QUIZZ (titreQ, descriptionQ, nb_questions) VALUES (:titre, :descri, :nb_q)";
+        $stmt = $db->prepare($sql);
+        $stmt->execute(array(':titre' => $titre, ':descri' => $description, ':nb_q' => $nb_questions));
+        if (!$stmt)
+            echo "Pb de requete";
+        else {
+            echo "Quizz ajouté";
+        }
         $id = $db->lastInsertId();
         echo "Quizz créé";
         header("Location: quizz_cree.php?id=$id");
